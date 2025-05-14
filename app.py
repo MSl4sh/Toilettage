158c682d4fd8831b839bd0c2022ae06a56616e7d
MrSlash <63912929+MSl4sh@users.noreply.github.com>
2025-05-14 16:39:08 +0200

    add services page

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request
 import psycopg2
+import xml.etree.ElementTree as ET
+from formulas import Formula
 
 
 
 app = Flask(__name__)
 
-
-# def get_connection():
-#         try:
-#             conn = psycopg2.connect(
-#                 dbname="start-pup",
-#                 user="postgres",
-#                 host="localhost",
-#                 password="postgres"
-#             )
-#             return conn
-#         except psycopg2.OperationalError as e:
-#             print(e)
-
-# get_connection()
-conn= psycopg2.connect("host=localhost dbname=start-pup user=postgres password=postgres port=5432")
-cur=conn.cursor()
-
-cur.execute(""" insert into appointment (appointment_date, hour_start)
-values ('2025-06-25', '08:00 AM'), ('2025-06-25', '10:00 AM'), ('2025-06-25', '01:00 PM'), ('2025-06-25', '03:00 PM')
-""")
-conn.commit()
-cur.close()
 
 
     
@@ -37,6 +17,50 @@ def default():
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route('/services')
+def services():
+    tree = ET.parse('data/formulas.xml')
+    root = tree.getroot()
+    
+    formulas = []
+    
+    
+    for package_elem in root.findall('package'):
+        package_id = package_elem.get('id')
+        title = package_elem.find('title').text
+        description = package_elem.find('description').text
+        price = package_elem.find('price').text
+        note = package_elem.find('note').text
+        features =[]
+        features.append(package_elem.find('first_feature').text)
+        features.append(package_elem.find('second_feature').text)
+        features.append(package_elem.find('third_feature').text)
+        features.append(package_elem.find('fourth_feature').text)
+        print(package_elem.find('first_feature').text)
+        formula = Formula(
+            package=package_id,
+            title=title,
+            description=description,
+            features=features,
+            price=price,
+            note=note
+        )
+        formulas.append(formula)
+
+    return render_template('services.html', formulas=formulas)
+
+@app.route("/test")
+def test():
+    conn = psycopg2.connect(
+        "host=localhost dbname=start-pup user=postgres password=postgres port=5432")
+    cur = conn.cursor()
+    cur.execute(""" insert into reservation (id_traitment, name, price, date_hour)
+    values ('1','Jacqueline',50, '2025-06-25 08:00 AM')
+    """)
+    conn.commit()
+    cur.close()
+    return redirect("/home")
 
 
 if __name__ == "__main__":
