9ead914979d10066aea06347765cdf068b6111d8
Xavier7500 <x.lechantre@gmail.com>
2025-05-14 13:17:42 +0200

    services

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, redirect, url_for, request
 
+import psycopg2
 
 app = Flask(__name__)
-if __name__ == "__main__":
-    app.run(debug=True)
+
     
-    
+conn = psycopg2.connect(host='localhost', dbname='Toiletries', user='postgres', password='Postgre', port=5432)
+cur = conn.cursor()
+cur.execute("""insert into reservation (name, price, date_hour)
+values ('Alain Terieur', 50, '2025-06-25')
+""")
+
+conn.commit()
+cur.close()
+conn.close()
+
+
 @app.route("/")
 def default():
     return redirect("/home")
@@ -13,3 +23,10 @@ def default():
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
